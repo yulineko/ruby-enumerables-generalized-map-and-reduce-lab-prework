ce2b4022b74b10f684bd20ee35516a1487a935d4
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-04 04:01:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def reduce(source_array, starting_value = 0)
     value = source_array[0]
     i = 1
   end
-  while i < source_array.length
+  while i < source_array.length do
     value = yield(value, source_array[i])
     i += 1
   end
